84338d4851858ca0dba1ddc2fa18e49d56b2feb1
Ken Esparta <nek.112358@gmail.com>
2016-07-14 05:13:13 -0300

    Colocando as considerações finais. Mudança no codigo de teste.

--- src_trabalho/cod_ml/test.m
+++ src_trabalho/cod_ml/test.m
@@ -1,9 +1,9 @@
 clc
 clear
 format long g;
-%parpool(4)
+parpool(2);
 tic;
-tam = 1000; percentagem_conhecida = .5; percentagem_treino = .1;
+tam = 1000; percentagem_conhecida = .3; percentagem_treino = .1;
 tam_con = tam * percentagem_conhecida;
 tam_treino = tam * percentagem_treino;
 sinal_grafo = carregar_grafo(tam);
@@ -21,7 +21,7 @@ e = 1; p = 1;% Elementos de treino
 num_clas = tam/10; tam_classe_con = tam_con/10; tam_class_tre = tam_treino/10;
 pos_conhecidas = zeros(1,tam_classe_con);
 pos_treino = zeros(1,tam_class_tre);
-for i=1:1
+for i=1:9
     pos_conhecidas(p : p + tam_classe_con - 1) = e : e + tam_classe_con - 1;
     pos_treino(p : p + tam_class_tre - 1) = e + tam_classe_con : e + tam_classe_con + tam_class_tre - 1;
     e = e + num_clas;
@@ -42,7 +42,14 @@ filtro = Filtro(sinal_grafo.matrizAdjacencia, 15, S_con, S_tre');
 
 %MSR = sqrt(sum((condicao - ones(tam,1)).^2));
 % w.mostra_grafo
-filtro.coeficientes;
-
+S_pred = zeros(1,tam);
+for i=1:tam
+    if filtro.S_pred(i) >= 0
+        S_pred(i) = 1;
+    else
+        S_pred(i) = -1;
+    end
+end
+stem(S_pred);
 toc;
-%delete(gcp)
+delete(gcp);
